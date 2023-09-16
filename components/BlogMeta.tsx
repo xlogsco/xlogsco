8ce860d4e6e967793f19f4dd988e5822a55b6706
xLogs.co <144883700+xlogsco@users.noreply.github.com>
2023-09-16 14:33:30 +0500

    change

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -27,7 +27,7 @@ export default function BlogMeta() {
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#0000" />
     </>
   )
 }
